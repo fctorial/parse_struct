da6a3033ca09b827f60fd74332ebd53d17088087
Sagar Tiwari <fctorial@gmail.com>
2020-03-22 09:12:10 +0530

    multimethods int-parsers

--- src/parse_struct/deserialize.clj
+++ src/parse_struct/deserialize.clj
@@ -1,35 +1,37 @@
 (ns parse_struct.deserialize
   (:require [parse_struct.utils :refer [split-n take-exactly pow pows2 bitCount type-size zip-colls]]
+            [parse_struct.common-types :refer :all]
             [clojure.spec.alpha :as s])
   (:import (java.nio ByteBuffer ByteOrder)))
 
-(defn parseInt64 [bytes]
-  (.getLong (.order (ByteBuffer/wrap (byte-array bytes)) ByteOrder/LITTLE_ENDIAN)))
+(defmacro make-int-parser [bits signed?]
+  (let [bc (/ bits 8)
+        getter ({8  '.get
+                 16 '.getShort
+                 32 '.getInt
+                 64 '.getLong} bits)
+        offset (pow 2 bits)
+        data-arg (gensym)
+        bb-var (gensym)
+        num-var (gensym)
+        sign-handler-exp (if signed?
+                           num-var
+                           `(if (neg? ~num-var)
+                              (+ ~num-var ~offset)
+                              ~num-var))]
+    `(fn [~data-arg]
+       (let [~bb-var (.order (ByteBuffer/wrap (byte-array (take-exactly ~bc ~data-arg))) ByteOrder/LITTLE_ENDIAN)
+             ~num-var (~getter ~bb-var)]
+         ~sign-handler-exp))))
 
-(defn parseInt32 [bytes]
-  (.getInt (.order (ByteBuffer/wrap (byte-array bytes)) ByteOrder/LITTLE_ENDIAN)))
-
-(defn parseInt16 [bytes]
-  (.getShort (.order (ByteBuffer/wrap (byte-array bytes)) ByteOrder/LITTLE_ENDIAN)))
-
-(defn parseInt8 [bytes]
-  (.get (ByteBuffer/wrap (byte-array bytes))))
-
-(defmacro make-unsigned-maker [bits]
-  (let [target-type (if (< bits 64)
-                      long
-                      bigint)
-        offset (pows2 bits)
-        numarg (gensym)]
-    `(fn [~numarg]
-       (if (< ~numarg 0)
-         (+ (~target-type ~numarg) ~offset)
-         ~numarg))))
-
-(def intParsers {1 [parseInt8 (make-unsigned-maker 8)]
-                 2 [parseInt16 (make-unsigned-maker 16)]
-                 4 [parseInt32 (make-unsigned-maker 32)]
-                 8 [parseInt64 (make-unsigned-maker 64)]})
+(def int-parsers {i8  (make-int-parser 8 true)
+                  i16 (make-int-parser 16 true)
+                  i32 (make-int-parser 32 true)
+                  i64 (make-int-parser 64 true)
+                  u8  (make-int-parser 8 false)
+                  u16 (make-int-parser 16 false)
+                  u32 (make-int-parser 32 false)
+                  u64 (make-int-parser 64 false)})
 
 (defmulti deserialize (fn [spec _] (spec :type)))
 
@@ -42,13 +44,8 @@
       (throw (new IllegalArgumentException "Floats can have 4 or 8 bytes")))))
 
 (defmethod deserialize :int
-  [{bc :bytes signed? :signed} data]
-  (let [[parser sign-handler] (intParsers bc)
-        parsedInt (parser (take-exactly bc data))
-        signHandled (if (not signed?)
-                      (sign-handler parsedInt)
-                      parsedInt)]
-    signHandled))
+  [spec data]
+  ((int-parsers spec) data))
 
 (defmethod deserialize :string
   [{bc :bytes trim_nulls? :trim_nulls} data]
--- test/benchmark.clj
+++ test/benchmark.clj
@@ -1,5 +1,6 @@
 (ns benchmark
   (:require [parse_struct.core :refer [serialize deserialize]]
+            [parse_struct.common-types :refer :all]
             [roundtrip_test :refer [gen-struct-val]]))
 
 (defn bench [f]
@@ -11,8 +12,28 @@
   (float (/ (reduce + coll)
             (count coll))))
 
+(def int-types [i8
+                i16
+                i32
+                i64
+                u8
+                u16
+                u32
+                u64])
+
+(defn reference-local [spec]
+  (update-in spec [:element :definition]
+             (fn [defs]
+               (map
+                 (fn [[n d]]
+                   (let [sec (first (filter #(= d %) int-types))]
+                     (if sec
+                       [n sec]
+                       [n d])))
+                 defs))))
+
 (defn -main []
-  (let [spec (read-string (slurp (str "test/big_spec.edn")))
+  (let [spec (reference-local (read-string (slurp (str "test/big_spec.edn"))))
         value (gen-struct-val spec)]
     (println "warming up")
     (doseq [i (range 1 10)]
