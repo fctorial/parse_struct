5ee39746d4001d3be4f7a5914301f9af2442231a
Sagar Tiwari <fctorial@gmail.com>
2020-03-22 07:20:45 +0530

    better benchmark

--- test/benchmark.clj
+++ test/benchmark.clj
@@ -2,13 +2,25 @@
   (:require [parse_struct.core :refer [serialize deserialize]]
             [roundtrip_test :refer [gen-struct-val]]))
 
+(defn bench [f]
+  (let [s (System/currentTimeMillis)]
+    (time (f))
+    (- (System/currentTimeMillis) s)))
+
+(defn avg [coll]
+  (float (/ (reduce + coll)
+            (count coll))))
+
 (defn -main []
   (let [spec (read-string (slurp (str "test/big_spec.edn")))
         value (gen-struct-val spec)]
     (println "warming up")
-    (doseq [i (range 1 4)]
+    (doseq [i (range 1 10)]
       (println i)
       (count (deserialize spec (serialize spec value))))
     (println "done")
-    (time (= (deserialize spec (serialize spec value))
-             (seq value)))))
+    (let [average (avg (for [_ (range 20)]
+                         (bench #(= (deserialize spec (serialize spec value))
+                                    (seq value)))))]
+      (println "average time: " average)
+      average)))
